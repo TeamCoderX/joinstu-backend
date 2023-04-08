a1716a1189ce09bb2728eff4a89f7a245adba788
Sianglife <siang7200.huang@gmail.com>
2023-04-08 15:18:08 +0800

    db columns optimization

--- routes/propose.js
+++ routes/propose.js
@@ -1,19 +1,19 @@
 var express = require('express');
 var router = express.Router();
-var { client, auth } = require('../lib/dbutils');
+var { client, auth, generateNewId } = require('../lib/dbutils');
 
 router.get('/comments/:id', (req, res) => {
     const id = req.params.id.toString()
     try {
         client.connect()
-        client.db('proposes-comment-support').collection(id).find().sort({date: -1}).toArray((err, supportData) => {
+        client.db('proposes-comment-support').collection(id).find().sort({ date: -1 }).toArray((err, supportData) => {
             if (err) throw err;
-            client.db('proposes-comment-else').collection(id).find().sort({date: -1}).toArray((err, elseData) => {
+            client.db('proposes-comment-else').collection(id).find().sort({ date: -1 }).toArray((err, elseData) => {
                 if (err) throw err;
-                res.json({support:supportData, else:elseData})
+                res.json({ support: supportData, else: elseData })
             })
         })
-    }catch(err){
+    } catch (err) {
         console.log(err)
     }
 })
@@ -35,10 +35,10 @@ router.post('/list', (req, res) => {
     try {
         client.db('main').collection('propose').find().sort({ date: -1 }).toArray((err, data) => {
             if (err) throw err;
-            if(data.length < ItemsPerPage || page==undefined){
-                res.json({pages: 1, data: data});
-            }else{
-                res.json({pages: Math.ceil((data.length)/ItemsPerPage), data:data.slice((page-1)*ItemsPerPage, page*ItemsPerPage)});
+            if (data.length < ItemsPerPage || page == undefined) {
+                res.json({ pages: 1, data: data });
+            } else {
+                res.json({ pages: Math.ceil((data.length) / ItemsPerPage), data: data.slice((page - 1) * ItemsPerPage, page * ItemsPerPage) });
             }
         })
     } catch (err) {
@@ -59,7 +59,7 @@ router.post('/support/:id', auth, (req, res) => {
                     client.db('main').collection('accounts').updateOne({ id: user }, { $push: { supportedProposes: proposeID } });
                 })
                 res.json({ status: true })
-            }else {
+            } else {
                 res.json({ status: false })
             }
         })
@@ -72,7 +72,7 @@ router.post('/support/:id', auth, (req, res) => {
 router.get('/supportStatus/:id', auth, (req, res) => {
     var user = req.session.user
     var proposeID = req.params.id
-    try{
+    try {
         client.db('main').collection('accounts').findOne({ id: user }, (err, data) => {
             if (err) throw err;
             if (data != null) {
@@ -85,20 +85,17 @@ router.get('/supportStatus/:id', auth, (req, res) => {
                 res.sendStatus(404)
             }
         })
-    }catch(err){
+    } catch (err) {
         console.log(err)
         res.sendStatus(500)
-    }    
+    }
 })
 
-function appendData(req) {
-    var randomURL = (Math.random() + 1).toString(10).substring(7);
-
-    client.db('main').collection('propose').findOne({ randomURL: randomURL }, (err, result) => {
-        if (err) throw err;
-        if (result == null) {
+router.post('/new', auth, (req, res) => {
+    try {
+        if (req.body.title == "" || req.body.details == "" || req.body.purpose == "") {
             client.db('main').collection('propose').insertOne({
-                url: randomURL,
+                url: generateNewId(req, 'main', 'propose'),
                 title: req.body.title,
                 details: req.body.details,
                 purpose: req.body.purpose,
@@ -106,21 +103,8 @@ function appendData(req) {
                 date: new Date,
                 num: 0
             })
-            client.db('main').collection('accounts').updateOne({ id: (req.session.user) }, { $push: { addedProposes: randomURL } });
-        } else {
-            return true;
-        }
-    })
-}
-
-router.post('/new', auth, (req, res) => {
-    try {
-        if(req.body.title == "" || req.body.details == "" || req.body.purpose == ""){
-            while (appendData(req)) {
-                appendData(req);
-            }
             res.json({ status: true });
-        }else{
+        } else {
             res.json({ status: false });
         }
     } catch (err) {
@@ -129,12 +113,13 @@ router.post('/new', auth, (req, res) => {
     }
 })
 
-router.post('/commentSupport/:id', auth, (req, res) => {    
+router.post('/commentSupport/:id', auth, (req, res) => {
     const id = req.params.id
     let user = req.session.user
-    if(req.body.contents != ""){
+    if (req.body.contents != "") {
         try {
             client.db('proposes-comment-support').collection(id).insertOne({
+                id: generateNewId(req, 'proposes-comment-support', id),
                 author: user,
                 contents: req.body.contents,
                 date: new Date
@@ -149,12 +134,13 @@ router.post('/commentSupport/:id', auth, (req, res) => {
     }
 })
 
-router.post('/commentElse/:id', auth, (req, res) => {     
+router.post('/commentElse/:id', auth, (req, res) => {
     const id = req.params.id
     let user = req.session.user
-    if(req.body.contents != ""){
+    if (req.body.contents != "") {
         try {
             client.db('proposes-comment-else').collection(id).insertOne({
+                id: generateNewId(req, 'proposes-comment-support', id),
                 author: user,
                 contents: req.body.contents,
                 date: new Date
